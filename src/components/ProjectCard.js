e494779d7d60e6f139612e51bb8f4a11ccdeb2d0
Anamitra Saikia <39421303+RicketyRusty@users.noreply.github.com>
2023-06-17 20:49:00 +0530

    ui/button-icon

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -15,7 +15,7 @@ const ProjectCard = ({ type, name, description, onClick, tags, links }) => {
     <div className={`w-[20rem] laptop:w-[40rem] overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link 
       border border-b-4 border-slate-700 shadow-lg hover:shadow-xl 
       transition-all ease-out duration-300 hover:scale-105 link 
-      ${mounted && theme === "dark" ? "hover:bg-neutral-950" : "hover:bg-slate-50"}`}
+      ${mounted && theme === "dark" ? "hover:bg-neutral-900" : "hover:bg-slate-50"}`}
     >
       <div className="p-4 rounded-lg flex items-center">
         <img className="w-8 h-8 mr-4 flex-shrink-0" src={`/images/${"sun.svg"}`}></img>
@@ -34,7 +34,7 @@ const ProjectCard = ({ type, name, description, onClick, tags, links }) => {
       <div className="px-2 pt-4 pb-2">
         {tags && tags.map((tag) => {
           return (
-            <span key={tag.id} className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <span key={tag.id} className="inline-block rounded-full border border-gray-500 px-3 py-1 text-sm mr-2 mb-2">
               {tag.name}
             </span>
           )
@@ -42,7 +42,7 @@ const ProjectCard = ({ type, name, description, onClick, tags, links }) => {
       </div>
       <div className={`flex flex-wrap mob:flex-nowrap link`}>
       {links && links.map((link, index) => (
-        <Button type="primary" key={index} onClick={() => window.open(link.url)}>
+        <Button type="withIcon" icon={link.title} key={index} onClick={() => window.open(link.url)}>
           {link.title}
         </Button>
       ))}
