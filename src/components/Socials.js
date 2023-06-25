423bfcf08867f286c62464863d6355fd7bf94c82
Anamitra Saikia <39421303+RicketyRusty@users.noreply.github.com>
2023-06-25 22:18:48 +0530

    fix/minor changes

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Button from "./Button";
 
-import yourData from "../data/portfolio.json";
+import data from "../data/portfolio.json";
 
 const Socials = ({ className }) => {
   return (
     <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
-      {yourData.socials.map((social, index) => (
+      {data.socials.map((social, index) => (
         <Button key={index} onClick={() => window.open(social.link)}>
           {social.title}
         </Button>
