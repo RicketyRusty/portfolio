ffe80d8fcadb8ceba3e87dca7cb7ad7ff1ecd377
Anamitra Saikia <39421303+anamitra-saikia@users.noreply.github.com>
2023-12-05 19:08:07 +0530

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import Button from "../components/Button";
+import { useRouter } from "next/router";
+export default function Custom404() {
+    const router = useRouter();
+    return(
+    <>
+        <div className="gradient-circle"></div>
+        <div className="gradient-circle-bottom"></div>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <p className="pb-8 text-2xl laptop:text-6xl">Error 404 😵‍💫</p>
+          <p className="py-8 text-xl laptop:text-4xl">Looks like you've taken a wrong turn</p>
+          <Button type="primary" onClick={() => router.push("/")}>Go To Home</Button>  
+        </div>
+    </>     
+    ) 
+}
